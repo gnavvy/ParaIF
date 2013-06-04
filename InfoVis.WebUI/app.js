0763e60e53642240a91dda14e16a439efc6a0712
Yang Wang <gnavvy@gmail.com>
2013-06-04 13:34:55 -0700

    some cleaning jobs

--- InfoVis.WebUI/app.js
+++ InfoVis.WebUI/app.js
@@ -1,63 +1,62 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
-var app = module.exports = express();
 
-app.configure(function () {
-    app.set('port', 3000 || process.env.PORT);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(app.router);
-    app.get('/', routes.index);
-    app.get('/critique', routes.critique);
-    app.get('/chris', routes.chris);
-    app.get('/svm', routes.svm);
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/views'));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-var server = http.createServer(app);
-var clients = require('now').initialize(server).now;
 var graph = require('./models/graph.server.js').graph;
 var gosper = require('./models/gosper.server.js').gosper;
 var host = '127.0.0.1', port = 4000;
 
-clients.start = function () {
-    this.now.reset(7, 4, 6);
-//    this.now.reset(19, 4, 12);
-};
+var app = module.exports = express(); {
+    app.configure(function () {
+        app.set('port', 3000 || process.env.PORT);
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.use(app.router);
+        app.get('/', routes.index);
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.static(__dirname + '/views'));
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    });
+}
 
-clients.reset = function (degree, order, n_clusters) {
-    console.log('reset with degree: ' + degree + ', order: ' + order);
-    graph.reset();
-    gosper.init(degree, order);
-    var opt_node = { host: host, port: port, path: '/getNodes' };
-    http.get(opt_node, function (res) {
-        var data = '';
-        res.on('data', function (chunk) { data += chunk; });
-        res.on('end', function () {
-            graph.setNodes(JSON.parse(data));
-            var opt_edge = { host: host, port: port, path: '/getEdges' };
-            http.get(opt_edge, function (res) {
-                data = '';
-                res.on('data', function (chunk) { data += chunk; });
-                res.on('end', function () {
-                    graph.setEdges(JSON.parse(data));
-                    graph.preprocess(degree, order, n_clusters);
-                    clients.setNodes(graph.getNodes());
-                    clients.setEdges(graph.getEdges());
-                    clients.setDegree(degree);
-                    clients.update();
+var server = http.createServer(app); {
+    server.listen(app.get('port'), function () {
+        console.log("Express server listening on port " + app.get('port'));
+    });
+}
+
+var clients = require('now').initialize(server).now; {
+    clients.start = function () {
+        this.now.reset(7, 4, 6);
+    };
+
+    clients.reset = function (degree, order, n_clusters) {
+        console.log('reset with degree: ' + degree + ', order: ' + order);
+        graph.reset();
+        gosper.init(degree, order);
+        var opt_node = { host: host, port: port, path: '/getNodes' };
+        http.get(opt_node, function (res) {
+            var data = '';
+            res.on('data', function (chunk) { data += chunk; });
+            res.on('end', function () {
+                graph.setNodes(JSON.parse(data));
+                var opt_edge = { host: host, port: port, path: '/getEdges' };
+                http.get(opt_edge, function (res) {
+                    data = '';
+                    res.on('data', function (chunk) { data += chunk; });
+                    res.on('end', function () {
+                        graph.setEdges(JSON.parse(data));
+                        graph.preprocess(degree, order, n_clusters);
+                        clients.setNodes(graph.getNodes());
+                        clients.setEdges(graph.getEdges());
+                        clients.setDegree(degree);
+                        clients.update();
+                    });
                 });
             });
         });
-    });
-};
-
-server.listen(app.get('port'), function () {    // go!
-    console.log("Express server listening on port " + app.get('port'));
-});
+    };
+}
--- InfoVis.WebUI/routes/index.js
+++ InfoVis.WebUI/routes/index.js
@@ -6,15 +6,3 @@
 exports.index = function(req, res) {
 	res.render('index', { title: 'ECS272 Information Visualization' })
 };
-
-exports.critique = function(req, res) {
-	res.render('critique', { title: 'Visualization Critique' })
-};
-
-exports.svm = function(req, res) {
-	res.render('svm', { title: 'SVM Visualization' })
-};
-
-exports.chris = function(req, res) {
-	res.render('chris', { title: 'd3.js Practice' })
-};
